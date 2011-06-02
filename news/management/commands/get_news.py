4c1384ef2220319b836f5079fa09fbf9e165c490
Hugo Chinchilla Carbonell <hugo@bulma.net>
2011-06-02 09:06:20 +0200

    Parser class refactored, added fixture with initial data, updated README

--- news/management/commands/get_news.py
+++ news/management/commands/get_news.py
@@ -1,6 +1,20 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.db.utils import IntegrityError
+
 from adsonews.news.models import New
 from adsonews.news.parsers import parsers
+
+_HEADER = '\033[95m'
+_OKBLUE = '\033[94m'
+_OKGREEN = '\033[92m'
+_WARNING = '\033[93m'
+_FAIL = '\033[91m'
+_ENDC = '\033[0m'
+
+green = lambda x: _OKGREEN + x + _ENDC
+blue = lambda x: _OKBLUE + x + _ENDC
+orange = lambda x: _WARNING + x + _ENDC
+red = lambda x: _FAIL + x + _ENDC
 
 class Command(BaseCommand):
     args = ''
@@ -9,6 +23,18 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         for parser in parsers:
             p = parser()
-            p.import_news()
-            
-        self.stdout.write('All done, have a nice day :)\n')
+            news = p.extract_entries()
+            for new in news:
+                self.load_entry(new)
+        
+        self.stdout.write(blue('All done, have a nice day :)\n'))
+
+    def load_entry(self, data):
+        try:
+            new = New(**data)
+            new.save();
+            self.stdout.write("%s %s\n" % (green('Nueva noticia:'), new.title))
+        except IntegrityError, e:
+            self.stdout.write("%s %s\n" % (orange('Noticia duplicada:'), new.title))
+        except:
+            self.stdout.write("%s %s\n" % (red('Error cargando noticia:'), new.title))
--- news/parsers/parser.py
+++ news/parsers/parser.py
@@ -1,10 +1,10 @@
 import re
 import urllib
+import HTMLParser
 from  datetime import datetime
 
 from django.template.defaultfilters import slugify
 from django.utils.html import strip_tags
-from django.db.utils import IntegrityError
 
 from news.models import New
 
@@ -23,6 +23,7 @@ red = lambda x: _FAIL + x + _ENDC
 class Parser(object):
     
     def __init__(self):
+        self.html_parser = HTMLParser.HTMLParser()
         self.site_name = None
         self.site_url = None
         self.site_slug = None
@@ -39,43 +40,43 @@ class Parser(object):
     def get_entry_regex(self):
         raise Exception('get_entry_regex is not implemented')
         
+    def get_content(self):
+        f = urllib.urlopen(self.feed_url)
+        content = f.read()
+        f.close()
+        return content.replace('\n','')
+        
+    def extract_entries(self, limit=10):
+        entries = self.get_entries(self.get_content())
+        return [self.parse_entry(entry) for entry in entries]
+        
     def parse_entry(self, new):
         match_object = self.entry_regex.search(new)
         data = match_object.groupdict()
         data['source_name'] = self.site_name
         data['source_url'] = self.site_url
         data['source_slug'] = slugify(self.site_name)
-        return data
+        return self.clean_data(data)
         
     def clean_data(self, data):
-        data['title'] = self.sanitize(data['title'])
-        data['body'] = self.sanitize(data['body'])
-        data['date'] = datetime.strptime(data['date'][:-6], '%a, %d %b %Y %H:%M:%S')
-        return data        
+        data['title'] = self.clean_title(data['title'])
+        data['body'] = self.clean_body(data['body'])
+        data['date'] = self.clean_date(data['date'])
+        return data
+    
+    def clean_date(self, string):
+        return datetime.strptime(string[:-6], '%a, %d %b %Y %H:%M:%S')
+        
+    def clean_body(self, string):
+        return self.sanitize(string)
+        
+    def clean_title(self, string):
+        return self.sanitize(string)
 
     def sanitize(self, content):
         cdata = self.cdata_regex.search(content)
         if cdata:
             content = cdata.groups()[0]
         content = strip_tags(content)
+        content = self.html_parser.unescape(content)
         return content
-        
-    def get_content(self):
-        f = urllib.urlopen(self.feed_url)
-        content = f.read()
-        f.close()
-        return content.replace('\n','')
-    
-    def import_news(self, limit=10):
-        entries = self.get_entries(self.get_content())
-        for entry in entries:
-            data = self.parse_entry(entry)
-            data = self.clean_data(data)
-            try:
-                new = New(**data)
-                new.save();
-                print "%s %s" % (green('Nueva noticia:'), new.title)
-            except IntegrityError, e:
-                print "%s %s" % (orange('Noticia duplicada:'), new.title)
-            except:
-                print "%s %s" % (red('Error cargando noticia:'), new.title)
